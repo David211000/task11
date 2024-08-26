06751ea60100f7637edb3afde2dbe4313cacc0a2
David211000 <dsrapyan@gmail.com>
2024-08-26 23:42:27 +0300

    Add files via upload

---
+++ task.py
@@ -0,0 +1,8 @@
+print('1st program')
+print(9**0.5*5)
+print('2nd program')
+print(9.99>9.98 and 1000!=100.1)
+print('3rd program')
+print(2*2+2==2*(2+2))
+print('4 th program')
+print(int(123.456*10)%10)
